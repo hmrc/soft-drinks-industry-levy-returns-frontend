b5e34861f17b37adadd9063302786b6211c8c467
Priyanka Dhiman <88194246+priyankadhiman3006@users.noreply.github.com>
2022-12-01 11:45:16 +0000

    DLS-6381 update yes no scaffolding

--- .g8/yesNoPage/migrations/$className__snake$.sh
+++ .g8/yesNoPage/migrations/$className__snake$.sh
@@ -16,6 +16,10 @@ echo "Adding messages to conf.messages"
 echo "" >> ../conf/messages.en
 echo "$className;format="decap"$.title = $className;format="decap"$" >> ../conf/messages.en
 echo "$className;format="decap"$.heading = $className;format="decap"$" >> ../conf/messages.en
+echo "$className;format="decap"$.subHeading = $className;format="decap"$" >> ../conf/messages.en
+echo "$className;format="decap"$.hint = $className;format="decap"$" >> ../conf/messages.en
+echo "$className;format="decap"$.detailsLink = $className;format="decap"$" >> ../conf/messages.en
+echo "$className;format="decap"$.detailsContent = $className;format="decap"$" >> ../conf/messages.en
 echo "$className;format="decap"$.checkYourAnswersLabel = $className;format="decap"$" >> ../conf/messages.en
 echo "$className;format="decap"$.error.required = Select yes if $className;format="decap"$" >> ../conf/messages.en
 echo "$className;format="decap"$.change.hidden = $className$" >> ../conf/messages.en
