a8f472d70e678794bc1aa28e13dddb7e7600969d
Priyanka Dhiman <88194246+priyankadhiman3006@users.noreply.github.com>
2022-12-15 16:09:10 +0000

    DLS-6607 integratiojn test added

---
+++ it/controllers/BrandsPackagedAtOwnSitesControllerIntegrationSpec.scala
@@ -0,0 +1,27 @@
+package controllers
+
+import controllers.testSupport.{Specifications, TestConfiguration}
+import play.api.test.WsTestClient
+
+class BrandsPackagedAtOwnSitesControllerIntegrationSpec extends Specifications with TestConfiguration {
+  "PackagedContractPackerController" should {
+    "Ask for many litres of liable drinks have user packaged at UK sites they operate" in {
+
+      given
+        .commonPrecondition
+
+      WsTestClient.withClient { client ⇒
+        val result1 = client.url(s"$baseUrl/how-many-own-brands-packaged-at-own-sites")
+          .withFollowRedirects(false)
+          //..addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+          .get()
+
+        whenReady(result1) { res ⇒
+          res.status mustBe 303
+        }
+
+      }
+    }
+  }
+
+}
