337c2ed3377ae4cd809ba6df7d5f99b77f428842
Priyanka Dhiman <88194246+priyankadhiman3006@users.noreply.github.com>
2022-12-20 16:11:30 +0000

    DLS-6637 test cases

--- .g8/multipleQuestionsDecimalPage/app/forms/$className$FormProvider.scala
+++ .g8/multipleQuestionsDecimalPage/app/forms/$className$FormProvider.scala
@@ -15,13 +15,15 @@ class $className$FormProvider @Inject() extends Mappings {
         "$className;format="decap"$.error.$field1Name$.required",
                     "$className;format="decap"$.error.$field1Name$.negative",
                     "$className;format="decap"$.error.$field1Name$.nonNumeric",
-                    "$className;format="decap"$.error.$field1Name$.wholeNumber")
+                    "$className;format="decap"$.error.$field1Name$.wholeNumber",
+  "$className;format="decap"$.error.$field1Name$.outOfMaxVal")
   .verifying(maximumValueNotEqual($field1Maximum$, "$className;format="decap"$.error.$field1Name$.outOfMaxVal")),
         "$field2Name$" -> long(
         "$className;format="decap"$.error.$field2Name$.required",
                     "$className;format="decap"$.error.$field2Name$.negative",
                     "$className;format="decap"$.error.$field2Name$.nonNumeric",
-                    "$className;format="decap"$.error.$field2Name$.wholeNumber")
+                    "$className;format="decap"$.error.$field2Name$.wholeNumber",
+  "$className;format="decap"$.error.$field2Name$.outOfMaxVal")
   .verifying(maximumValueNotEqual($field2Maximum$, "$className;format="decap"$.error.$field2Name$.outOfMaxVal"))
     )($className$.apply)($className$.unapply)
    )
