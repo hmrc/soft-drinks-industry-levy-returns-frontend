1850b5ceee5c69ec1c104c7b52062a68fb283253
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2022-12-20 16:30:35 +0000

    [DLS-6612] PR fixes

--- it/controllers/testSupport/HowManyCreditsForLostDamagedIntergrationSpec.scala
+++ it/controllers/testSupport/HowManyCreditsForLostDamagedIntergrationSpec.scala
@@ -1,6 +1,7 @@
 package controllers.testSupport
 
 import controllers.testSupport.{Specifications, TestConfiguration}
+import play.api.libs.ws.DefaultWSCookie
 import play.api.test.WsTestClient
 
 class HowManyCreditsForLostDamagedIntergrationSpec extends Specifications with TestConfiguration {
@@ -13,11 +14,11 @@ class HowManyCreditsForLostDamagedIntergrationSpec extends Specifications with T
       WsTestClient.withClient { client ⇒
         val result1 = client.url(s"$baseUrl/how-many-credits-for-lost-damaged")
           .withFollowRedirects(false)
-          //..addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+          .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
           .get()
 
         whenReady(result1) { res ⇒
-          res.status mustBe 303
+          res.status mustBe 200
         }
 
       }
