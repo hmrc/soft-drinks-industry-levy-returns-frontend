94aa441473f8fdeb2b289f878573770969306159
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2022-12-30 10:09:29 +0000

    [DLS-6528] Adding Intergration Test

--- it/controllers/AddASmallProducerControllerIntergrationSpec.scala
+++ it/controllers/AddASmallProducerControllerIntergrationSpec.scala
@@ -4,15 +4,15 @@ import controllers.testSupport.{Specifications, TestConfiguration}
 import play.api.libs.ws.DefaultWSCookie
 import play.api.test.WsTestClient
 
-class AskSecondaryWarehouseInReturnControllerIntegrationSpec extends Specifications with TestConfiguration {
-  "AskSecondaryWarehouseInReturnController" should {
-    "Ask the user to enter the registered small producer's details" in {
+class AddASmallProducerControllerIntergrationSpec extends Specifications with TestConfiguration {
+  "AddASmallProducerController" should {
+    "Ask for if user wants to register any UK warehouses where user used to store liable drinks" in {
 
       given
         .commonPrecondition
 
       WsTestClient.withClient { client ⇒
-        val result1 = client.url(s"$baseUrl/add-small-producer")
+        val result1 = client.url(s"$baseUrl/ask-secondary-warehouses-in-return")
           .withFollowRedirects(false)
           .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
           .get()
@@ -26,4 +26,3 @@ class AskSecondaryWarehouseInReturnControllerIntegrationSpec extends Specificati
   }
 
 }
-
