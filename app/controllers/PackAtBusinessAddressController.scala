6946e0e718e01077ceb46dc45f778e3bd901cd76
Ginny Thomas <90277886+ginnyamazed@users.noreply.github.com>
2023-02-22 10:44:37 +0000

    DLS-6716 connect to controller

--- app/controllers/PackAtBusinessAddressController.scala
+++ app/controllers/PackAtBusinessAddressController.scala
@@ -16,10 +16,13 @@
 
 package controllers
 
+import connectors.SoftDrinksIndustryLevyConnector
 import controllers.actions._
 import forms.PackAtBusinessAddressFormProvider
+
 import javax.inject.Inject
 import models.Mode
+import models.backend.UkAddress
 import navigation.Navigator
 import pages.PackAtBusinessAddressPage
 import play.api.i18n.{I18nSupport, MessagesApi}
@@ -28,12 +31,15 @@ import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.PackAtBusinessAddressView
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 class PackAtBusinessAddressController @Inject()(
                                          override val messagesApi: MessagesApi,
                                          sessionRepository: SessionRepository,
                                          navigator: Navigator,
+                                         connector: SoftDrinksIndustryLevyConnector,
                                          identify: IdentifierAction,
                                          getData: DataRetrievalAction,
                                          requireData: DataRequiredAction,
@@ -46,13 +52,17 @@ class PackAtBusinessAddressController @Inject()(
 
   def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
     implicit request =>
+      val subscription = Await.result(connector.retrieveSubscription(request.userAnswers.id,"sdil"),1.seconds)
 
+      val businessName = subscription.get.orgName
+      val businessAddress = subscription.get.address
+
+      println(Console.YELLOW + "This is my test" + businessName + Console.WHITE)
       val preparedForm = request.userAnswers.get(PackAtBusinessAddressPage) match {
         case None => form
         case Some(value) => form.fill(value)
       }
-
-      Ok(view(preparedForm, mode))
+      Ok(view(preparedForm, businessName, businessAddress, mode))
   }
 
   def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
@@ -60,13 +70,13 @@ class PackAtBusinessAddressController @Inject()(
 
       form.bindFromRequest().fold(
         formWithErrors =>
-          Future.successful(BadRequest(view(formWithErrors, mode))),
+          Future.successful(BadRequest(view(formWithErrors, businessName: String, businessAddress: UkAddress, mode))),
 
         value =>
           for {
             updatedAnswers <- Future.fromTry(request.userAnswers.set(PackAtBusinessAddressPage, value))
             _              <- sessionRepository.set(updatedAnswers)
-          } yield Redirect(navigator.nextPage(PackAtBusinessAddressPage, mode, updatedAnswers))
+          } yield Redirect(navigator.nextPage(BrandsPackagedAtOwnSitesController, mode, updatedAnswers))
       )
   }
 }
