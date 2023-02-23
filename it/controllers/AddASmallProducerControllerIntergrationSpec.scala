123f4ee6f1be6b0f3cb94c4b0117e6abb834b30a
Ginny Thomas <90277886+ginnyamazed@users.noreply.github.com>
2023-02-23 11:25:53 +0000

    warningCleanUp update files to correct The unicode arrow `⇒` is deprecated, use `=>` instead warnings in ITs

--- it/controllers/AddASmallProducerControllerIntergrationSpec.scala
+++ it/controllers/AddASmallProducerControllerIntergrationSpec.scala
@@ -15,7 +15,7 @@ class AddASmallProducerControllerIntergrationSpec extends Specifications with Te
       given
         .commonPrecondition
 
-      WsTestClient.withClient { client ⇒
+      WsTestClient.withClient { client =>
         val result1 = client.url(s"$baseUrl/add-small-producer")
           .withFollowRedirects(false)
           .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
