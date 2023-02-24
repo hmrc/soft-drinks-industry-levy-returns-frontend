e50883cfc288ef69ab240b53ca80476528f023c6
Ramin Esfandiari <ramin.esfan@gmail.com>
2023-02-24 14:43:41 +0000

    DLS-6532: brought into uk levy lines CYA

--- test/controllers/CheckYourAnswersControllerSpec.scala
+++ test/controllers/CheckYourAnswersControllerSpec.scala
@@ -276,11 +276,11 @@ class CheckYourAnswersControllerSpec extends SpecBase with SummaryListFluency wi
         page.getElementsByTag("dt").text() must include(Messages("lowBandLevy"))
         page.getElementsByTag("dd").text() must include("£1800")
 
-//        page.getElementsByTag("dt").text() must include(Messages("litresInTheHighBand"))
-//        page.getElementsByTag("dd").text() must include("20000")
-//        page.getElementById("change-highband-literage").attributes().get("href") mustEqual s"$baseUrl/change-how-many-brought-into-uk"
-//        page.getElementsByTag("dt").text() must include(Messages("highBandLevy"))
-//        page.getElementsByTag("dd").text() must include("£4800")
+        page.getElementsByTag("dt").text() must include(Messages("litresInTheHighBand"))
+        page.getElementsByTag("dd").text() must include("20000")
+        page.getElementById("change-highband-literage-brought-into-uk").attributes().get("href") mustEqual s"$baseUrl/change-how-many-brought-into-uk"
+        page.getElementsByTag("dt").text() must include(Messages("highBandLevy"))
+        page.getElementsByTag("dd").text() must include("£4800")
       }
     }
 
