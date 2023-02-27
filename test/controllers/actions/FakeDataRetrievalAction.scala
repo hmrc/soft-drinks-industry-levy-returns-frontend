9f3319d9562f3be06627afae9a38c79dbf0070d2
Ginny Thomas <90277886+ginnyamazed@users.noreply.github.com>
2023-02-27 11:57:14 +0000

    DLS-6716 update FakeDataRetrieval from dataRetrieved to userAnswers

--- test/controllers/actions/FakeDataRetrievalAction.scala
+++ test/controllers/actions/FakeDataRetrievalAction.scala
@@ -24,7 +24,7 @@ import scala.concurrent.{ExecutionContext, Future}
 class FakeDataRetrievalAction(userAnswers: Option[UserAnswers], returnPeriod: Option[ReturnPeriod]) extends DataRetrievalAction {
 
   override protected def transform[A](request: IdentifierRequest[A]): Future[OptionalDataRequest[A]] =
-    Future(OptionalDataRequest(request.request, request.sdilEnrolment, request.subscription, dataToReturn, request.returnPeriod))
+    Future(OptionalDataRequest(request.request, request.sdilEnrolment, request.subscription, userAnswers, request.returnPeriod))
 
 
   override protected implicit val executionContext: ExecutionContext =
