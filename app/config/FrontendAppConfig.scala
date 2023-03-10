d4d07dee1a3ed39b3171912674af634083da96ab
Ginny Thomas <90277886+ginnyamazed@users.noreply.github.com>
2023-03-10 14:28:43 +0000

    DLS-6996 remove out code present for another test branch not needed for this ticket

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -17,43 +17,26 @@
 package config
 
 import com.google.inject.{Inject, Singleton}
-import config.features.Features
-import config.{ConfigKeys => Keys}
+import play.api.Configuration
 import play.api.i18n.Lang
 import play.api.mvc.RequestHeader
-import play.api.{Configuration, Environment}
 import uk.gov.hmrc.play.bootstrap.binders.SafeRedirectUrl
-import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
-
-trait AppConfig {
-  val features: Features
-  val addressLookupCallbackUrl: String
-  val timeoutPeriod: Int
-  val timeoutCountdown: Int
-  def feedbackUrl: String
-  def addressLookupService: String
-  val accessibilityReportUrl: String
-  // val addressLookupUrlHost: String
-  val signInContinueBaseUrl: String
-  val deskproServiceName: String
-
-}
 
 @Singleton
-class FrontendAppConfig @Inject()(implicit configuration: Configuration, servicesConfig: ServicesConfig, environment: Environment) extends AppConfig {
+class FrontendAppConfig @Inject() (configuration: Configuration) {
 
-  val host: String = configuration.get[String]("host")
+  val host: String    = configuration.get[String]("host")
   val appName: String = configuration.get[String]("appName")
 
   private val contactHost = configuration.get[String]("contact-frontend.host")
   private val contactFormServiceIdentifier = "soft-drinks-industry-levy-returns-frontend"
 
-  override lazy val feedbackUrl: String =
-    s"$contactHost/contact/beta-feedback?service=$contactFormServiceIdentifier&backUrl=${SafeRedirectUrl(host + exitSurveyBaseUrl).encodedUrl}"
+  def feedbackUrl(implicit request: RequestHeader): String =
+    s"$contactHost/contact/beta-feedback?service=$contactFormServiceIdentifier&backUrl=${SafeRedirectUrl(host + request.uri).encodedUrl}"
 
-  val loginUrl: String = configuration.get[String]("urls.login")
+  val loginUrl: String         = configuration.get[String]("urls.login")
   val loginContinueUrl: String = configuration.get[String]("urls.loginContinue")
-  val signOutUrl: String = configuration.get[String]("urls.signOut")
+  val signOutUrl: String       = configuration.get[String]("urls.signOut")
 
   private val exitSurveyBaseUrl: String = configuration.get[Service]("microservice.services.feedback-frontend").baseUrl
   val exitSurveyUrl: String = s"$exitSurveyBaseUrl/feedback/soft-drinks-industry-levy-returns-frontend"
@@ -66,40 +49,11 @@ class FrontendAppConfig @Inject()(implicit configuration: Configuration, service
     "cy" -> Lang("cy")
   )
 
-  val timeout: Int = configuration.get[Int]("timeout-dialog.timeout")
+  val timeout: Int   = configuration.get[Int]("timeout-dialog.timeout")
   val countdown: Int = configuration.get[Int]("timeout-dialog.countdown")
 
   val cacheTtl: Int = configuration.get[Int]("mongodb.timeToLiveInSeconds")
 
-  override val features = new Features
-
-  override def addressLookupService: String = {
-    if (features.stubAddressLookup()) {
-      host + "/vat-through-software/account/test-only/address-lookup-stub"
-    } else {
-      servicesConfig.baseUrl(Keys.addressLookupFrontend)
-    }
-  }
-
-  //  override def agentClientLookupUrl: String =
-  //    if (features.stubAgentClientLookup()) {
-  //      testOnly.controllers.routes.StubAgentClientLookupController.show(controllers.routes.CustomerCircumstanceDetailsController.show.url).url
-  //    } else {
-  //      vatAgentClientLookupHandoff(controllers.routes.CustomerCircumstanceDetailsController.show.url)
-  //    }
-
-  override lazy val addressLookupCallbackUrl: String =
-    signInContinueBaseUrl + servicesConfig.getString(Keys.addressLookupCallback)
-
-  override lazy val signInContinueBaseUrl: String = servicesConfig.getString(Keys.signInContinueBaseUrl)
-
-  override lazy val timeoutPeriod: Int = servicesConfig.getInt(Keys.timeoutPeriod)
-  override lazy val timeoutCountdown: Int = servicesConfig.getInt(Keys.timeoutCountDown)
-
-  override lazy val accessibilityReportUrl: String = servicesConfig.getString(Keys.accessibilityReportUrl)
-  override lazy val deskproServiceName: String = servicesConfig.getString(Keys.deskproServiceName)
-
   val lowerBandCostPerLitre: BigDecimal = BigDecimal(configuration.get[String]("lowerBandCostPerLitre"))
   val higherBandCostPerLitre: BigDecimal = BigDecimal(configuration.get[String]("higherBandCostPerLitre"))
-
 }
