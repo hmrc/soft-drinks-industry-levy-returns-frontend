3fac116e40a8a23561b904a43e97660e6646673e
Ginny Thomas <90277886+ginnyamazed@users.noreply.github.com>
2023-03-29 16:57:15 +0100

    DLS-6718 clean up current related warnings

--- app/controllers/PackagingSiteDetailsController.scala
+++ app/controllers/PackagingSiteDetailsController.scala
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
-import config.FrontendAppConfig
 import connectors.SoftDrinksIndustryLevyConnector
 import controllers.actions._
 import forms.PackagingSiteDetailsFormProvider
@@ -32,7 +47,7 @@ class PackagingSiteDetailsController @Inject()(
                                                 formProvider: PackagingSiteDetailsFormProvider,
                                                 val controllerComponents: MessagesControllerComponents,
                                                 view: PackagingSiteDetailsView
-                                              )(implicit ec: ExecutionContext, config: FrontendAppConfig) extends FrontendBaseController with I18nSupport {
+                                              )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
   val form = formProvider()
 
--- app/viewmodels/checkAnswers/PackagingSiteDetailsSummary.scala
+++ app/viewmodels/checkAnswers/PackagingSiteDetailsSummary.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package viewmodels.checkAnswers
 
 import controllers.routes
@@ -11,7 +27,6 @@ import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.{Key, SummaryListRow}
 import viewmodels.govuk.summarylist._
 import viewmodels.implicits._
-import scala.language.postfixOps
 
 object PackagingSiteDetailsSummary {
 
@@ -63,7 +78,7 @@ object PackagingSiteDetailsSummary {
     }
   }
 
-  private def addressFormatting(site: Site)(implicit messages: Messages): String = {
+  private def addressFormatting(site: Site): String = {
     val commaFormattedSiteAddress = site.address.lines.map(line => { if (line.isEmpty) "" else line + ", " })
 
     val addressNoTradingName = {
