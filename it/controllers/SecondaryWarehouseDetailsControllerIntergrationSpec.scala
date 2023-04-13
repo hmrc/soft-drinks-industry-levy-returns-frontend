13ae499f70429d0453e9b0b0b3363e1c88f3ba1f
Ginny Thomas <90277886+ginnyamazed@users.noreply.github.com>
2023-04-13 14:36:54 +0100

    DLS-7522 update failing IT tests with set userAnswers

--- it/controllers/SecondaryWarehouseDetailsControllerIntergrationSpec.scala
+++ it/controllers/SecondaryWarehouseDetailsControllerIntergrationSpec.scala
@@ -1,13 +1,15 @@
 package controllers
 
-import controllers.testSupport.{Specifications, TestConfiguration}
+import controllers.testSupport.{ITCoreTestData, Specifications, TestConfiguration}
+import org.scalatest.TryValues
 import play.api.libs.ws.DefaultWSCookie
 import play.api.test.WsTestClient
 
-class SecondaryWarehouseDetailsControllerIntergrationSpec extends Specifications with TestConfiguration {
+class SecondaryWarehouseDetailsControllerIntergrationSpec extends Specifications with TestConfiguration with ITCoreTestData with TryValues {
   "SecondaryWarehouseDetailsController" should {
     "Ask for if user wants to add more warehouses" in {
 
+      setAnswers(newPackerPartialAnswers.success.value)
       given
         .commonPrecondition
 
