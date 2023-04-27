622ca23d9036d2b61c3cd170328ab6a6dd5f6371
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2023-04-27 08:58:38 +0100

    [DLS-7859] Fixing Unit Tests

--- test/connectors/AddressLookupConnectorSpec.scala
+++ test/connectors/AddressLookupConnectorSpec.scala
@@ -20,8 +20,9 @@ import base.SpecBase
 import models.Address
 import play.api.http.Status
 import play.api.test.Helpers.{await, defaultAwaitTimeout}
-import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpResponse}
+import uk.gov.hmrc.http.{BadRequestException, HeaderCarrier, HttpClient, HttpException, HttpResponse}
 import connectors.httpParsers.ResponseHttpParser.HttpResult
+import mocks.MockHttp
 import models.core.ErrorModel
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
@@ -30,14 +31,15 @@ import org.scalatestplus.mockito.MockitoSugar
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{ExecutionContext, Future}
 
-class AddressLookupConnectorSpec extends SpecBase with MockitoSugar{
+class AddressLookupConnectorSpec extends SpecBase with MockitoSugar with MockHttp {
 
   val errorModel: HttpResponse = HttpResponse(Status.BAD_REQUEST, "Error Message")
-  val mockHttp = mock[HttpClient]
   val TestAddressLookupConnector = new AddressLookupConnector(mockHttp,frontendAppConfig)
   val addressLookupConnector = new AddressLookupConnector(http =mockHttp, frontendAppConfig)
   implicit val hc = HeaderCarrier()
   implicit val ec = ExecutionContext
+
+
 
   "AddressLookupConnector" - {
 
@@ -50,31 +52,21 @@ class AddressLookupConnectorSpec extends SpecBase with MockitoSugar{
 
     "for getAddress method" - {
 
+      def getAddressResult: Future[HttpResult[Address]] = TestAddressLookupConnector.getAddress(vrn)
+
       "called for a Right with CustomerDetails" - {
 
         "return a CustomerAddressModel" in {
-          when(mockHttp.GET[HttpResult[Address]](any(), any(), any())(any(), any(), any())).thenReturn(Future.successful(Right(customerAddressMax)))
-          val res = TestAddressLookupConnector.getAddress(vrn)
-          whenReady(
-            res
-          ){
-            response =>
-              response mustEqual Right(customerAddressMax)
-          }
+          setupMockHttpGet(TestAddressLookupConnector.getAddressUrl(vrn))(Right(customerAddressMax))
+          await(getAddressResult) mustBe Right(customerAddressMax)
         }
       }
 
       "given an error should" - {
 
         "return an Left with an ErrorModel" in {
-          when(mockHttp.GET[HttpResult[Address]](any(), any(), any())(any(), any(), any())).thenReturn(Future.failed(new RuntimeException))
-          val res = TestAddressLookupConnector.getAddress(vrn)
-          whenReady(
-            res
-          ){
-            response =>
-              response mustEqual new RuntimeException
-          }
+          setupMockHttpGet(TestAddressLookupConnector.getAddressUrl(vrn))(Left(errorModel))
+          await(getAddressResult) mustBe Left(errorModel)
         }
       }
     }
