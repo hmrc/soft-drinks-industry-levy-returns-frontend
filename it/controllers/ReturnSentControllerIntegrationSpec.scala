04befe51f8dd1bafcbbdf2f30b9b6b0200742663
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2023-05-17 13:59:51 +0100

    [DLS-7831] Adding extra test to ensure blocking and access works based on submission

--- it/controllers/ReturnSentControllerIntegrationSpec.scala
+++ it/controllers/ReturnSentControllerIntegrationSpec.scala
@@ -8,6 +8,26 @@ import play.api.test.WsTestClient
 class ReturnSentControllerIntegrationSpec extends Specifications with TestConfiguration with  ITCoreTestData with TryValues {
 
   "ReturnSentController" should {
+
+    "Show the user there submitted information after successfully submitting their return" in {
+      val userAnswers = checkYourAnswersFullAnswers
+      setAnswers(userAnswers.copy(submitted = true))
+      given
+        .commonPrecondition
+
+      WsTestClient.withClient { client =>
+        val result1 = client.url(s"$baseUrl/return-sent")
+          .withFollowRedirects(false)
+          .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+          .get()
+
+        whenReady(result1) { res =>
+          res.status mustBe 200
+        }
+
+      }
+    }
+
     "Redirect to beginning of journey when no returns sent" in {
       val userAnswers = smallProducerDetaisPartialAnswers.success.value
       setAnswers(userAnswers.copy(submitted = false))
