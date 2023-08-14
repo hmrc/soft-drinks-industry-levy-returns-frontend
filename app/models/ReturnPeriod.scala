d8bfcdbff02f723a265ca8f3fbaf2011c1964020
Christine Stokes <18244373+Chrissystk@users.noreply.github.com>
2023-08-14 10:27:55 +0100

    DLS-7972-CS updated returnPeriod model (#163)

--- app/models/ReturnPeriod.scala
+++ app/models/ReturnPeriod.scala
@@ -23,20 +23,32 @@ import java.time.LocalDate
 case class ReturnPeriod(year: Int, quarter: Int) {
   require(quarter <= 3 && quarter >= 0)
   require(year >= 2018)
-  def start: LocalDate = LocalDate.of(year, quarter * 3 + 1, if (count == 0) 5 else 1)
+
+  def start: LocalDate = {
+    val dayOfWeek = if (year == 2018 && quarter == 1) {
+      5
+    } else {
+      1
+    }
+    LocalDate.of(year, quarter * 3 + 1, dayOfWeek)
+  }
+
   def end: LocalDate = next.start.minusDays(1)
+
   def deadline: LocalDate = end.plusDays(30)
-  def next: ReturnPeriod = ReturnPeriod(count + 1)
-  def previous: ReturnPeriod = ReturnPeriod(count - 1)
-  def count: Int = year * 4 + quarter - 2018 * 4 - 1
+
+  def next: ReturnPeriod = {
+    val nextReturnQuarter = (quarter + 1) % 4
+    val nextReturnYear = if (nextReturnQuarter < quarter) {
+      year + 1
+    } else {
+      year
+    }
+    ReturnPeriod(nextReturnYear, nextReturnQuarter)
+  }
 }
 
 object ReturnPeriod {
-  def apply(o: Int): ReturnPeriod = {
-    val i = o + 1
-    ReturnPeriod(2018 + i / 4, i % 4)
-  }
-
   def apply(date: LocalDate): ReturnPeriod = ReturnPeriod(date.getYear, quarter(date))
   def quarter(date: LocalDate): Int = { date.getMonthValue - 1 } / 3
   implicit val format = Json.format[ReturnPeriod]
