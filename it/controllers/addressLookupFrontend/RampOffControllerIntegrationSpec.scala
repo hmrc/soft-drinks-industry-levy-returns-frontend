67c9ae614bdab2c560d198f434e825e7c4859481
Chrissystk <18244373+Chrissystk@users.noreply.github.com>
2023-09-21 10:11:27 +0100

    DLS-8874 change from sdilId to siteId and added checkroute for warehouse

--- it/controllers/addressLookupFrontend/RampOffControllerIntegrationSpec.scala
+++ it/controllers/addressLookupFrontend/RampOffControllerIntegrationSpec.scala
@@ -1,7 +1,7 @@
 package controllers.addressLookupFrontend
 
 import controllers.testSupport.{ITCoreTestData, Specifications, TestConfiguration}
-import models.NormalMode
+import models.{CheckMode, NormalMode}
 import models.backend.{Site, UkAddress}
 import org.scalatest.TryValues
 import play.api.http.Status.{INTERNAL_SERVER_ERROR, SEE_OTHER}
@@ -12,185 +12,190 @@ import services.{PackingDetails, WarehouseDetails}
 
 class RampOffControllerIntegrationSpec extends Specifications with TestConfiguration with ITCoreTestData with TryValues {
 
-  s"ramp off $WarehouseDetails" should {
-    "redirect to next page when request is valid and address is returned from ALF" when {
-      "no address exists in DB currently for SDILID provided" in {
-        val siteId: String = "foo"
-        val alfId: String = "bar"
-        setUpData(emptyUserAnswers)
-        given
-          .commonPreconditionChangeSubscription(aSubscription)
-          .alf.getAddress(alfId)
+  val modes = List(NormalMode, CheckMode)
 
-        WsTestClient.withClient { client =>
-          val result = client.url(s"$baseUrl/off-ramp/secondary-warehouses/$siteId?id=$alfId")
-            .withFollowRedirects(false)
-            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
-            .get()
+  val siteId: String = "foo"
+  val alfId: String = "bar"
 
-          whenReady(result) { res =>
-            val updatedUserAnswers = getAnswers(emptyUserAnswers.id).get
-            updatedUserAnswers.id mustBe emptyUserAnswers.id
-            updatedUserAnswers.data mustBe emptyUserAnswers.data
-            updatedUserAnswers.packagingSiteList mustBe emptyUserAnswers.packagingSiteList
-            updatedUserAnswers.submitted mustBe emptyUserAnswers.submitted
-            updatedUserAnswers.smallProducerList mustBe emptyUserAnswers.smallProducerList
-            updatedUserAnswers.warehouseList mustBe Map(siteId -> Site(UkAddress(List("line 1", "line 2", "line 3", "line 4"), "aa1 1aa", alfId = Some(alfId)), tradingName = Some("soft drinks ltd")))
-
-            res.status mustBe SEE_OTHER
-            res.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.SecondaryWarehouseDetailsController.onPageLoad(NormalMode).url)
-          }
-
-        }
+  modes.foreach { case mode =>
+    s"ramp off $WarehouseDetails when in $mode" should {
+      val path = if(mode == NormalMode) {
+        s"$baseUrl/off-ramp/secondary-warehouses/$siteId?id=$alfId"
+      } else {
+        s"$baseUrl/off-ramp/change-secondary-warehouses/$siteId?id=$alfId"
       }
-      "an address already exists in DB currently for SDILID provided" in {
-        val siteId: String = "foo"
-        val alfId: String = "bar"
-        val userAnswersBefore = emptyUserAnswers.copy(warehouseList = Map(siteId -> Site(UkAddress(List.empty, "foo", Some("wizz")), tradingName = None)))
-        setUpData(userAnswersBefore)
-        given
-          .commonPreconditionChangeSubscription(aSubscription)
-          .alf.getAddress(alfId)
+      "redirect to next page when request is valid and address is returned from ALF" when {
+        "no address exists in DB currently for SDILID provided" in {
+          setUpData(emptyUserAnswers)
+          given
+            .commonPreconditionChangeSubscription(aSubscription)
+            .alf.getAddress(alfId)
 
-        WsTestClient.withClient { client =>
-          val result = client.url(s"$baseUrl/off-ramp/secondary-warehouses/$siteId?id=$alfId")
-            .withFollowRedirects(false)
-            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
-            .get()
+          WsTestClient.withClient { client =>
+            val result = client.url(path)
+              .withFollowRedirects(false)
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .get()
 
-          whenReady(result) { res =>
-            val updatedUserAnswers = getAnswers(emptyUserAnswers.id).get
-            updatedUserAnswers.id mustBe emptyUserAnswers.id
-            updatedUserAnswers.data mustBe emptyUserAnswers.data
-            updatedUserAnswers.packagingSiteList mustBe emptyUserAnswers.packagingSiteList
-            updatedUserAnswers.submitted mustBe emptyUserAnswers.submitted
-            updatedUserAnswers.smallProducerList mustBe emptyUserAnswers.smallProducerList
-            updatedUserAnswers.warehouseList mustBe Map(siteId -> Site(UkAddress(List("line 1", "line 2", "line 3", "line 4"), "aa1 1aa", alfId = Some(alfId)), tradingName = Some("soft drinks ltd")))
+            whenReady(result) { res =>
+              val updatedUserAnswers = getAnswers(emptyUserAnswers.id).get
+              updatedUserAnswers.id mustBe emptyUserAnswers.id
+              updatedUserAnswers.data mustBe emptyUserAnswers.data
+              updatedUserAnswers.packagingSiteList mustBe emptyUserAnswers.packagingSiteList
+              updatedUserAnswers.submitted mustBe emptyUserAnswers.submitted
+              updatedUserAnswers.smallProducerList mustBe emptyUserAnswers.smallProducerList
+              updatedUserAnswers.warehouseList mustBe Map(siteId -> Site(UkAddress(List("line 1", "line 2", "line 3", "line 4"), "aa1 1aa", alfId = Some(alfId)), tradingName = Some("soft drinks ltd")))
 
-            res.status mustBe SEE_OTHER
-            res.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.SecondaryWarehouseDetailsController.onPageLoad(NormalMode).url)
+              res.status mustBe SEE_OTHER
+              res.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.SecondaryWarehouseDetailsController.onPageLoad(mode).url)
+            }
+
           }
         }
-      }
-    }
-    s"return $INTERNAL_SERVER_ERROR" when {
-      "alf returns error" in {
-        val siteId: String = "foo"
-        val alfId: String = "bar"
-        setUpData(emptyUserAnswers)
-        given
-          .commonPreconditionChangeSubscription(aSubscription)
-          .alf.getBadAddress(alfId)
+        "an address already exists in DB currently for SDILID provided" in {
+          val userAnswersBefore = emptyUserAnswers.copy(warehouseList = Map(siteId -> Site(UkAddress(List.empty, "foo", Some("wizz")), tradingName = None)))
+          setUpData(userAnswersBefore)
+          given
+            .commonPreconditionChangeSubscription(aSubscription)
+            .alf.getAddress(alfId)
 
-        WsTestClient.withClient { client =>
-          val result = client.url(s"$baseUrl/off-ramp/secondary-warehouses/$siteId?id=$alfId")
-            .withFollowRedirects(false)
-            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
-            .get()
+          WsTestClient.withClient { client =>
+            val result = client.url(path)
+              .withFollowRedirects(false)
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .get()
 
-          whenReady(result) { res =>
-            res.status mustBe INTERNAL_SERVER_ERROR
+            whenReady(result) { res =>
+              val updatedUserAnswers = getAnswers(emptyUserAnswers.id).get
+              updatedUserAnswers.id mustBe emptyUserAnswers.id
+              updatedUserAnswers.data mustBe emptyUserAnswers.data
+              updatedUserAnswers.packagingSiteList mustBe emptyUserAnswers.packagingSiteList
+              updatedUserAnswers.submitted mustBe emptyUserAnswers.submitted
+              updatedUserAnswers.smallProducerList mustBe emptyUserAnswers.smallProducerList
+              updatedUserAnswers.warehouseList mustBe Map(siteId -> Site(UkAddress(List("line 1", "line 2", "line 3", "line 4"), "aa1 1aa", alfId = Some(alfId)), tradingName = Some("soft drinks ltd")))
 
-            val updatedUserAnswers = getAnswers(emptyUserAnswers.id).get
-            updatedUserAnswers.id mustBe emptyUserAnswers.id
-            updatedUserAnswers.data mustBe emptyUserAnswers.data
-            updatedUserAnswers.packagingSiteList mustBe emptyUserAnswers.packagingSiteList
-            updatedUserAnswers.submitted mustBe emptyUserAnswers.submitted
-            updatedUserAnswers.smallProducerList mustBe emptyUserAnswers.smallProducerList
-            updatedUserAnswers.warehouseList mustBe emptyUserAnswers.warehouseList
+              res.status mustBe SEE_OTHER
+              res.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.SecondaryWarehouseDetailsController.onPageLoad(mode).url)
+            }
           }
         }
       }
-    }
-  }
-  s"ramp off $PackingDetails" should {
-    "redirect to next page when request is valid and address is returned from ALF" when {
-      "no address exists in DB currently for SDILID provided" in {
-        val siteId: String = "foo"
-        val alfId: String = "bar"
-        setUpData(emptyUserAnswers)
-        given
-          .commonPreconditionChangeSubscription(aSubscription)
-          .alf.getAddress(alfId)
+      s"return $INTERNAL_SERVER_ERROR" when {
+        "alf returns error" in {
+          setUpData(emptyUserAnswers)
+          given
+            .commonPreconditionChangeSubscription(aSubscription)
+            .alf.getBadAddress(alfId)
 
-        WsTestClient.withClient { client =>
-          val result = client.url(s"$baseUrl/off-ramp/packing-site-details/$siteId?id=$alfId")
-            .withFollowRedirects(false)
-            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
-            .get()
+          WsTestClient.withClient { client =>
+            val result = client.url(path)
+              .withFollowRedirects(false)
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .get()
 
-          whenReady(result) { res =>
-            val updatedUserAnswers = getAnswers(emptyUserAnswers.id).get
-            updatedUserAnswers.id mustBe emptyUserAnswers.id
-            updatedUserAnswers.data mustBe emptyUserAnswers.data
-            updatedUserAnswers.packagingSiteList mustBe Map(siteId ->
-              Site(UkAddress(List("line 1", "line 2", "line 3", "line 4"), "aa1 1aa", alfId = Some(alfId)), None, Some("soft drinks ltd"), None))
-            updatedUserAnswers.submitted mustBe emptyUserAnswers.submitted
-            updatedUserAnswers.smallProducerList mustBe emptyUserAnswers.smallProducerList
-            updatedUserAnswers.warehouseList mustBe emptyUserAnswers.warehouseList
+            whenReady(result) { res =>
+              res.status mustBe INTERNAL_SERVER_ERROR
 
-            res.status mustBe SEE_OTHER
-            res.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.PackagingSiteDetailsController.onPageLoad(NormalMode).url)
-          }
-
-        }
-      }
-      "an address already exists in DB currently for SDILID provided" in {
-        val siteId: String = "foo"
-        val alfId: String = "bar"
-        val userAnswersBefore = emptyUserAnswers.copy(packagingSiteList = Map(siteId -> Site(UkAddress(List.empty, "foo", Some("wizz")), None, None, None)))
-        setUpData(userAnswersBefore)
-        given
-          .commonPreconditionChangeSubscription(aSubscription)
-          .alf.getAddress(alfId)
-
-        WsTestClient.withClient { client =>
-          val result = client.url(s"$baseUrl/off-ramp/packing-site-details/$siteId?id=$alfId")
-            .withFollowRedirects(false)
-            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
-            .get()
-
-          whenReady(result) { res =>
-            val updatedUserAnswers = getAnswers(emptyUserAnswers.id).get
-            updatedUserAnswers.id mustBe emptyUserAnswers.id
-            updatedUserAnswers.data mustBe emptyUserAnswers.data
-            updatedUserAnswers.packagingSiteList mustBe Map(siteId ->
-              Site(UkAddress(List("line 1", "line 2", "line 3", "line 4"), "aa1 1aa", alfId = Some(alfId)), None, Some("soft drinks ltd"), None))
-            updatedUserAnswers.submitted mustBe emptyUserAnswers.submitted
-            updatedUserAnswers.smallProducerList mustBe emptyUserAnswers.smallProducerList
-            updatedUserAnswers.warehouseList mustBe emptyUserAnswers.warehouseList
-
-            res.status mustBe SEE_OTHER
-            res.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.PackagingSiteDetailsController.onPageLoad(NormalMode).url)
+              val updatedUserAnswers = getAnswers(emptyUserAnswers.id).get
+              updatedUserAnswers.id mustBe emptyUserAnswers.id
+              updatedUserAnswers.data mustBe emptyUserAnswers.data
+              updatedUserAnswers.packagingSiteList mustBe emptyUserAnswers.packagingSiteList
+              updatedUserAnswers.submitted mustBe emptyUserAnswers.submitted
+              updatedUserAnswers.smallProducerList mustBe emptyUserAnswers.smallProducerList
+              updatedUserAnswers.warehouseList mustBe emptyUserAnswers.warehouseList
+            }
           }
         }
       }
     }
-    s"return $INTERNAL_SERVER_ERROR" when {
-      "alf returns error" in {
-        val siteId: String = "foo"
-        val alfId: String = "bar"
-        setUpData(emptyUserAnswers)
-        given
-          .commonPreconditionChangeSubscription(aSubscription)
-          .alf.getBadAddress(alfId)
+    s"ramp off $PackingDetails when in $mode" should {
+      val path = if (mode == NormalMode) {
+        s"$baseUrl/off-ramp/packing-site-details/$siteId?id=$alfId"
+      } else {
+        s"$baseUrl/off-ramp/change-packing-site-details/$siteId?id=$alfId"
+      }
+      "redirect to next page when request is valid and address is returned from ALF" when {
+        "no address exists in DB currently for SDILID provided" in {
+          setUpData(emptyUserAnswers)
+          given
+            .commonPreconditionChangeSubscription(aSubscription)
+            .alf.getAddress(alfId)
 
-        WsTestClient.withClient { client =>
-          val result = client.url(s"$baseUrl/off-ramp/packing-site-details/$siteId?id=$alfId")
-            .withFollowRedirects(false)
-            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
-            .get()
+          WsTestClient.withClient { client =>
+            val result = client.url(path)
+              .withFollowRedirects(false)
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .get()
 
-          whenReady(result) { res =>
-            res.status mustBe INTERNAL_SERVER_ERROR
+            whenReady(result) { res =>
+              val updatedUserAnswers = getAnswers(emptyUserAnswers.id).get
+              updatedUserAnswers.id mustBe emptyUserAnswers.id
+              updatedUserAnswers.data mustBe emptyUserAnswers.data
+              updatedUserAnswers.packagingSiteList mustBe Map(siteId ->
+                Site(UkAddress(List("line 1", "line 2", "line 3", "line 4"), "aa1 1aa", alfId = Some(alfId)), None, Some("soft drinks ltd"), None))
+              updatedUserAnswers.submitted mustBe emptyUserAnswers.submitted
+              updatedUserAnswers.smallProducerList mustBe emptyUserAnswers.smallProducerList
+              updatedUserAnswers.warehouseList mustBe emptyUserAnswers.warehouseList
 
-            val updatedUserAnswers = getAnswers(emptyUserAnswers.id).get
-            updatedUserAnswers.id mustBe emptyUserAnswers.id
-            updatedUserAnswers.data mustBe emptyUserAnswers.data
-            updatedUserAnswers.packagingSiteList mustBe emptyUserAnswers.packagingSiteList
-            updatedUserAnswers.submitted mustBe emptyUserAnswers.submitted
-            updatedUserAnswers.smallProducerList mustBe emptyUserAnswers.smallProducerList
-            updatedUserAnswers.warehouseList mustBe emptyUserAnswers.warehouseList
+              res.status mustBe SEE_OTHER
+              res.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.PackagingSiteDetailsController.onPageLoad(mode).url)
+            }
+
+          }
+        }
+        "an address already exists in DB currently for SDILID provided" in {
+          val userAnswersBefore = emptyUserAnswers.copy(packagingSiteList = Map(siteId -> Site(UkAddress(List.empty, "foo", Some("wizz")), None, None, None)))
+          setUpData(userAnswersBefore)
+          given
+            .commonPreconditionChangeSubscription(aSubscription)
+            .alf.getAddress(alfId)
+
+          WsTestClient.withClient { client =>
+            val result = client.url(path)
+              .withFollowRedirects(false)
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .get()
+
+            whenReady(result) { res =>
+              val updatedUserAnswers = getAnswers(emptyUserAnswers.id).get
+              updatedUserAnswers.id mustBe emptyUserAnswers.id
+              updatedUserAnswers.data mustBe emptyUserAnswers.data
+              updatedUserAnswers.packagingSiteList mustBe Map(siteId ->
+                Site(UkAddress(List("line 1", "line 2", "line 3", "line 4"), "aa1 1aa", alfId = Some(alfId)), None, Some("soft drinks ltd"), None))
+              updatedUserAnswers.submitted mustBe emptyUserAnswers.submitted
+              updatedUserAnswers.smallProducerList mustBe emptyUserAnswers.smallProducerList
+              updatedUserAnswers.warehouseList mustBe emptyUserAnswers.warehouseList
+
+              res.status mustBe SEE_OTHER
+              res.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.PackagingSiteDetailsController.onPageLoad(mode).url)
+            }
+          }
+        }
+      }
+      s"return $INTERNAL_SERVER_ERROR" when {
+        "alf returns error" in {
+          setUpData(emptyUserAnswers)
+          given
+            .commonPreconditionChangeSubscription(aSubscription)
+            .alf.getBadAddress(alfId)
+
+          WsTestClient.withClient { client =>
+            val result = client.url(path)
+              .withFollowRedirects(false)
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .get()
+
+            whenReady(result) { res =>
+              res.status mustBe INTERNAL_SERVER_ERROR
+
+              val updatedUserAnswers = getAnswers(emptyUserAnswers.id).get
+              updatedUserAnswers.id mustBe emptyUserAnswers.id
+              updatedUserAnswers.data mustBe emptyUserAnswers.data
+              updatedUserAnswers.packagingSiteList mustBe emptyUserAnswers.packagingSiteList
+              updatedUserAnswers.submitted mustBe emptyUserAnswers.submitted
+              updatedUserAnswers.smallProducerList mustBe emptyUserAnswers.smallProducerList
+              updatedUserAnswers.warehouseList mustBe emptyUserAnswers.warehouseList
+            }
           }
         }
       }
