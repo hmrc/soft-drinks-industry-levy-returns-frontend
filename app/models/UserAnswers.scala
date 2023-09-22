a96f1ef419a93b725cc581906ebd063d46f92c01
Chrissystk <18244373+Chrissystk@users.noreply.github.com>
2023-09-22 15:06:45 +0100

    DLS-8894

--- app/models/UserAnswers.scala
+++ app/models/UserAnswers.scala
@@ -67,20 +67,7 @@ case class UserAnswers(
     }
   }
 
-  def addPackagingSite(site: Site, siteId: String): UserAnswers = {
-    copy(
-      packagingSiteList = packagingSiteList.filterNot(_._1 == siteId) ++ Map(siteId -> site)
-    )
-  }
-
-  def addWarehouse(site: Site, siteId: String): UserAnswers = {
-    copy(
-      warehouseList = warehouseList.filterNot(_._1 == siteId) ++ Map(siteId -> site)
-    )
-  }
-
-
-    def setAndRemoveLitresIfReq(page: Settable[Boolean], litresPage: Settable[LitresInBands], value: Boolean)
+  def setAndRemoveLitresIfReq(page: Settable[Boolean], litresPage: Settable[LitresInBands], value: Boolean)
                              (implicit writes: Writes[Boolean]): Try[UserAnswers] = {
     set(page, value).map { updatedAnswers =>
       if (value) {
