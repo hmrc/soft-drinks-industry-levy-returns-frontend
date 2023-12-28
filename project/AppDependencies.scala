9c7f5f446eb2154745a983a57cd7e0fa5077ef7c
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2023-12-28 13:47:46 +0000

    [DLS-9665] Ammedning versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,30 +5,30 @@ object AppDependencies {
 
 
   val playSuffix = s"-play-28"
+  val bootstrapVersion = "7.22.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.19.0-play-28",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
-    "uk.gov.hmrc"       %% s"bootstrap-frontend$playSuffix" % "7.21.0",
-    "org.typelevel"     %% "cats-core"                      % "2.9.0",
-    "uk.gov.hmrc.mongo" %% s"hmrc-mongo$playSuffix"         % "1.3.0",
-    "uk.gov.hmrc"       %% s"crypto-json$playSuffix"        % "7.3.0"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.19.0-play-28",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.13.0-play-28",
+    "uk.gov.hmrc"       %% s"bootstrap-frontend$playSuffix" % bootstrapVersion,
+    "org.typelevel"     %% "cats-core"                     % "2.9.0",
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo$playSuffix"           % "1.6.0",
+    "uk.gov.hmrc"       %% s"crypto-json$playSuffix"        % "7.6.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"           %% "scalatest"                    % "3.2.16",
-    "org.scalatestplus"       %% "scalacheck-1-15"              % "3.2.11.0",
-    "org.scalatestplus"       %% "mockito-3-4"                  % "3.2.10.0",
+    "org.scalatest"            %% "scalatest"                    % "3.2.17"   ,
+    "org.scalacheck"           %% "scalacheck"                   % "1.17.0"   ,
+    "org.scalatestplus"        %% "scalacheck-1-17"              % "3.2.14.0",
     "org.scalatestplus.play"  %% "scalatestplus-play"           % "5.1.0",
-    "org.pegdown"             %  "pegdown"                      % "1.6.0",
-    "org.jsoup"               %  "jsoup"                        % "1.15.4",
-    "com.typesafe.play"       %% "play-test"                    % PlayVersion.current,
-    "org.mockito"             %% "mockito-scala"                % "1.17.14",
-    "org.scalacheck"          %% "scalacheck"                   % "1.17.0",
-    "uk.gov.hmrc"             %% s"bootstrap-test$playSuffix"   % "7.21.0",
-    "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-test$playSuffix"  % "1.3.0",
-    "com.vladsch.flexmark"    %  "flexmark-all"                 % "0.62.0",
+    "org.pegdown"              %  "pegdown"                      % "1.6.0",
+    "org.jsoup"                %  "jsoup"                        % "1.15.4",
+    "com.typesafe.play"        %% "play-test"                    % PlayVersion.current,
+    "org.mockito"              %% "mockito-scala"                % "1.17.14",
+    "uk.gov.hmrc"              %% s"bootstrap-test$playSuffix"   % bootstrapVersion,
+    "uk.gov.hmrc.mongo"        %% s"hmrc-mongo-test$playSuffix"  % "1.6.0",
+    "com.vladsch.flexmark"      %  "flexmark-all"                 % "0.64.8",
     "com.github.tomakehurst"  % "wiremock-standalone"           % "2.27.2",
     "io.github.wolfendale"    %% "scalacheck-gen-regexp"        % "1.1.0"
   ).map(_ % "test, it")
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -6,13 +6,13 @@ resolvers += "Typesafe Releases" at "https://repo.typesafe.com/typesafe/releases
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.17.0")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.2.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.4.0")
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.20")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.9.0")
 
-addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0" exclude("org.scala-lang.modules", "scala-xml_2.12"))
 
-addSbtPlugin("org.irundaia.sbt" % "sbt-sassify" % "1.5.1")
+addSbtPlugin("io.github.irundaia" % "sbt-sassify" % "1.5.2")
 
 addSbtPlugin("net.ground5hark.sbt" % "sbt-concat" % "0.2.0")
 
@@ -21,4 +21,4 @@ addSbtPlugin("com.typesafe.sbt" % "sbt-uglify" % "2.0.0")
 addSbtPlugin("com.typesafe.sbt" % "sbt-digest" % "1.1.4")
 
 ThisBuild / libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
-addSbtPlugin("org.scoverage"     % "sbt-scoverage"      % "2.0.7")
+addSbtPlugin("org.scoverage"     % "sbt-scoverage"      % "2.0.9")
