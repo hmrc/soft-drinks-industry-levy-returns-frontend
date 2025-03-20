23ab7953092cd1a3d37123f72cd9a78994101ef5
emanfalola9998 <77966019+emanfalola9998@users.noreply.github.com>
2025-03-20 15:02:02 +0000

    removed sbt-digest plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,16 +3,16 @@ import sbt._
 object AppDependencies {
 
   private val playVersion = "play-30"
-  private val bootstrapVersion = "9.7.0"
-  private val hmrcMongoVersion = "2.4.0"
+  private val bootstrapVersion = "9.11.0"
+  private val hmrcMongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"            % "11.11.0",
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"            % "11.12.0",
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion"            % bootstrapVersion,
     "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVersion" % "3.2.0",
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"                    % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% s"crypto-json-$playVersion"                   % "8.1.0",
+    "uk.gov.hmrc"       %% s"crypto-json-$playVersion"                   % "8.2.0",
     "org.typelevel"     %% "cats-core"                                   % "2.12.0"
   )
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -6,7 +6,7 @@ resolvers += "Typesafe Releases" at "https://repo.typesafe.com/typesafe/releases
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.24.0")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.5.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.6.0")
 
 addSbtPlugin("org.playframework" % "sbt-plugin" % "3.0.6")
 
@@ -16,9 +16,7 @@ addSbtPlugin("io.github.irundaia" % "sbt-sassify" % "1.5.2")
 
 addSbtPlugin("com.github.sbt" % "sbt-concat" % "1.0.0")
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-uglify" % "2.0.0")
-
-addSbtPlugin("com.github.sbt" % "sbt-digest" % "2.0.0")
+addSbtPlugin("com.github.sbt" % "sbt-uglify" % "3.0.1")
 
 addSbtPlugin("org.scalariform" % "sbt-scalariform" % "1.8.3" exclude("org.scala-lang.modules", "scala-xml_2.12"))
 
